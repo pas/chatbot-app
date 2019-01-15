b735886ea29cbfa9d994d5a1babac3b3a39bab28
Pas <pascal_zaugg@web.de>
2019-01-15 10:02:13 +0100

    Adding some comments to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # The paths are specific to my current environment.
-# Please change them for your needs. 
+# Please change them for your needs.
 
 gnome-terminal --command "python -m rasa_core_sdk.endpoint --actions actions" --working-directory=/home/pascal/Documents/chatbot-app/rasa
 gnome-terminal --command "python -m rasa_core.run --enable_api --cors '*' -d models/dialogue -u models/current/nlu --endpoints endpoints.yml --debug --port 5002 --credentials credentials.yml" --working-directory=/home/pascal/Documents/chatbot-app/rasa
